e0a21394506abf637e455b5375bc53da380e6f68
Yaqiang <yaqiang.wang@gmail.com>
2020-09-27 23:55:07 +0800

    add native library path at run time

--- miml/__init__.py
+++ miml/__init__.py
@@ -1,10 +1,16 @@
 import sys
 import os
+from org.meteoinfo.util import PathUtil
 
-libdir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'lib')
-fns = os.listdir(libdir)
+base_dir = os.path.dirname(os.path.abspath(__file__))
+native_lib_dir = os.path.join(base_dir, 'native_lib')
+if os.path.exists(native_lib_dir):
+    PathUtil.addLibraryPath(native_lib_dir)
+
+lib_dir = os.path.join(base_dir, 'lib')
+fns = os.listdir(lib_dir)
 for fn in fns:
-    fpath = os.path.join(libdir, fn)
+    fpath = os.path.join(lib_dir, fn)
     if os.path.isfile(fpath) and fn.endswith('.jar'):
         if not fpath in sys.path:
             sys.path.append(fpath)
