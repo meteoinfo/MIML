df9b2ca17983cb0fe77d5027f40b74f0579a9ecf
Yaqiang <yaqiang.wang@gmail.com>
2020-09-28 13:12:43 +0800

    update smile to version 2.5.3

--- miml/__init__.py
+++ miml/__init__.py
@@ -3,10 +3,6 @@ import os
 from org.meteoinfo.util import PathUtil
 
 base_dir = os.path.dirname(os.path.abspath(__file__))
-native_lib_dir = os.path.join(base_dir, 'native_lib')
-if os.path.exists(native_lib_dir):
-    PathUtil.addLibraryPath(native_lib_dir)
-
 lib_dir = os.path.join(base_dir, 'lib')
 fns = os.listdir(lib_dir)
 for fn in fns:
